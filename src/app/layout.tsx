408ca69487b6c01f7709f3f5a70cb21d302792af
truuue <noahvernhet@gmail.com>
2024-10-27 18:18:38 +0100

    Push all the base files of the project include next.js basic configuration, prettier, tailwindcss and shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Créateur de templates",
+  description: "Outils de création de templates personnalisés",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Créateur de templates</h1>
+      <p>Créez des templates personnalisés pour vos projets.</p>
+
+      <div>
+        <h2>Créer un template</h2>
+      </div>
+    </div>
+  );
+}
