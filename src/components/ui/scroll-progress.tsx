082768c0f0884d698b2e92050a074f07596c8991
truuue <noahvernhet@gmail.com>
2024-10-28 12:37:42 +0100

    change the scroll progress index

--- src/components/ui/scroll-progress.tsx
+++ src/components/ui/scroll-progress.tsx
@@ -12,7 +12,7 @@ export function ScrollProgress() {
 
   return (
     <motion.div
-      className="fixed left-0 right-0 top-0 z-50 h-1 origin-left bg-primary"
+      className="fixed left-0 right-0 top-0 z-[1000] h-1 origin-left bg-primary"
       style={{ scaleX }}
     />
   );
