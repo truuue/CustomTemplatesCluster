f9b2f36f5103f6a39fca582921f51ee9704f0ed2
Noah <57148467+truuue@users.noreply.github.com>
2025-01-12 17:42:13 +0000

    format

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export function Loading() {
   return (
-    <div className="flex min-h-[200px] h-screen flex-col items-center justify-center">
+    <div className="flex h-screen min-h-[200px] flex-col items-center justify-center">
       <Loader2 className="h-8 w-8 animate-spin text-primary" />
       <p className="mt-2 text-gray-600">Chargement...</p>
     </div>
