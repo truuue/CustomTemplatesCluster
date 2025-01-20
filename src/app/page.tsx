fdfea3f9d3c57cdc7002f012543be68885819729
truuue <noahvernhet@gmail.com>
2025-01-20 21:33:35 +0100

    use cookies for toaster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import FAQ from "@/components/home/faq";
 import Features from "@/components/home/features";
 import FeaturesMobile from "@/components/home/features-mobile";
@@ -9,48 +11,64 @@ import HowItWorksMobile from "@/components/home/howitworks-mobile";
 import Pricing from "@/components/home/pricing";
 import Testimonials from "@/components/home/testimonials";
 import BackgroundGrid from "@/components/ui/background-grid";
-import { ScrollProgress } from "@/components/ui/scroll-progress";
+import { useToast } from "@/hooks/use-toast";
+import { cookies } from "@/lib/cookies";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { toast } = useToast();
+  const searchParams = useSearchParams();
+  const loginSuccess = searchParams?.get("login");
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated" && !cookies.hasShownLoginToast()) {
+      toast({
+        title: "Connexion réussie",
+        description: "Vous êtes maintenant connecté",
+      });
+      cookies.setLoginToast();
+    }
+  }, [status, toast]);
+
   return (
-    <>
-      <ScrollProgress />
-      <div className="flex min-h-[100dvh] flex-col">
-        {/* Header */}
-        <Header />
+    <div className="flex min-h-[100dvh] flex-col">
+      {/* Header */}
+      <Header />
 
-        <main className="relative flex-1 pt-[64px] lg:pt-0">
-          <BackgroundGrid />
+      <main className="relative flex-1 pt-[64px] lg:pt-0">
+        <BackgroundGrid />
 
-          <div className="z-10 space-y-12">
-            {/* Hero Section */}
-            <Hero />
+        <div className="z-10 space-y-12">
+          {/* Hero Section */}
+          <Hero />
 
-            {/* Features Section avec animation */}
-            <Features />
-            <FeaturesMobile />
+          {/* Features Section avec animation */}
+          <Features />
+          <FeaturesMobile />
 
-            {/* Comment ça marche Section */}
-            <HowItWorks />
-            <HowItWorksMobile />
+          {/* Comment ça marche Section */}
+          <HowItWorks />
+          <HowItWorksMobile />
 
-            {/* Pricing Section avec animation */}
-            <Pricing />
+          {/* Pricing Section avec animation */}
+          <Pricing />
 
-            {/* Testimonials Section avec animation */}
-            <Testimonials />
+          {/* Testimonials Section avec animation */}
+          <Testimonials />
 
-            {/* Statistiques Section */}
-            {/* <Stats /> */}
+          {/* Statistiques Section */}
+          {/* <Stats /> */}
 
-            {/* Questions Fréquentes Section */}
-            <FAQ />
-          </div>
-        </main>
+          {/* Questions Fréquentes Section */}
+          <FAQ />
+        </div>
+      </main>
 
-        {/* Footer */}
-        <Footer />
-      </div>
-    </>
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
