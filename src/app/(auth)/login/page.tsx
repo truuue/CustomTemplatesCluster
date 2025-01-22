ca63723532ae455e9140c75d2d1efb89ebf8565d
truuue <noahvernhet@gmail.com>
2025-01-22 17:21:43 +0100

    Try to fix useSearchParams with suspence

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useState } from "react";
 
-export default function LoginPage() {
+function LoginForm() {
   const [isLoading, setIsLoading] = useState(false);
   const searchParams = useSearchParams();
   const error = searchParams?.get("error");
@@ -25,38 +25,44 @@ export default function LoginPage() {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="container flex h-screen w-screen flex-col items-center justify-center">
-        <Card className="w-full max-w-md">
-          <CardHeader className="text-center">
-            <CardTitle className="text-2xl">Bienvenue sur Showcaser</CardTitle>
-            {error && (
-              <p className="mt-2 text-sm text-red-500">
-                Une erreur est survenue lors de la connexion
-              </p>
-            )}
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={() => handleSignIn("github")}
-              disabled={isLoading}
-            >
-              Continuer avec GitHub
-            </Button>
+    <Card className="w-full max-w-md">
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl">Bienvenue sur Showcaser</CardTitle>
+        {error && (
+          <p className="mt-2 text-sm text-red-500">
+            Une erreur est survenue lors de la connexion
+          </p>
+        )}
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <Button
+          variant="outline"
+          className="w-full"
+          onClick={() => handleSignIn("github")}
+          disabled={isLoading}
+        >
+          Continuer avec GitHub
+        </Button>
 
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={() => handleSignIn("google")}
-              disabled={isLoading}
-            >
-              Continuer avec Google
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
-    </Suspense>
+        <Button
+          variant="outline"
+          className="w-full"
+          onClick={() => handleSignIn("google")}
+          disabled={isLoading}
+        >
+          Continuer avec Google
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function LoginPage() {
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Suspense fallback={<div>Chargement...</div>}>
+        <LoginForm />
+      </Suspense>
+    </div>
   );
 }
