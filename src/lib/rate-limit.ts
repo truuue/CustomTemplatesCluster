fa193435fe4a5a4766071f23eeb527dcb03b9ed2
truuue <noahvernhet@gmail.com>
2025-01-27 11:09:35 +0100

    upgrade a little the rate limit

--- src/lib/rate-limit.ts
+++ src/lib/rate-limit.ts
@@ -2,19 +2,23 @@ import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 
-export async function rateLimit(request: Request) {
+export async function rateLimit(request: Request, limit = 100, duration = 60) {
   const ip = request.headers.get("x-forwarded-for") ?? "127.0.0.1";
-  const key = `rate-limit:${ip}`;
+  const path = request.url.split("/")[1] || "default";
+  const key = `rate-limit:${ip}:${path}`;
 
   const current = await redis.incr(key);
-  if (current > 100) {
-    // 100 requêtes par minute
+  if (current > limit) {
     return {
       success: false,
-      message: "Trop de requêtes",
+      message: "Trop de requêtes veuillez réessayer plus tard",
+      remaining: 0,
     };
   }
 
-  await redis.expire(key, 60);
-  return { success: true };
+  await redis.expire(key, duration);
+  return {
+    success: true,
+    remaining: limit - current,
+  };
 }
