155a4165cb131c9c102f8edca6841695f2be1a9e
truuue <noahvernhet@gmail.com>
2025-02-03 18:26:07 +0100

    Refactor privacy page data collection rendering

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -38,15 +38,13 @@ export default function PrivacyPage() {
             {siteConfig.legal.privacy.dataCollection.intro}
           </p>
           <ul className="list-inside list-disc space-y-2 text-muted-foreground">
-            {siteConfig.legal.privacy.dataCollection.items
-              ? siteConfig.legal.privacy.dataCollection.items.map(
-                  (item: string, index: number) => (
-                    <li key={index}>
-                      {item.replace(/'/g, "&apos;").replace(/"/g, "&quot;")}
-                    </li>
-                  )
-                )
-              : null}
+            {siteConfig.legal.privacy.dataCollection.items.map(
+              (value, index) => (
+                <li key={index}>
+                  {value.replace(/'/g, "&apos;").replace(/"/g, "&quot;")}
+                </li>
+              )
+            )}
           </ul>
         </section>
 
